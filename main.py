171040c487848430c4d6bafd1fdd7428c675571d
Maksym Stoliarov <Windmill78max@gmail.com>
2024-02-08 13:34:56 +0100

    Pc name added for multiple devices

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ import os
 import telebot
 from dotenv import load_dotenv
 
+load_dotenv(override=True)
+
 battery = psutil.sensors_battery()
 percent = int(battery.percent)
 status = battery.power_plugged
+pc_name = os.getenv('PC_NAME')
 
-message = f"Battery percent is {percent}%"
+message = f"{pc_name} battery percent is {percent}%"
 print(message)
 
 if percent <= 20 and status is False:
-    load_dotenv(override=True)
     bot_key = os.getenv('BOT_KEY')
     chat_id = os.getenv('CHAT_ID')
 
